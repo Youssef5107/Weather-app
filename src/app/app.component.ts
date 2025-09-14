8612bf9fbc51392d6ba75420b20e0a0b57661bb1
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-14 21:06:43 +0300

    feat: add access location function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,29 +13,37 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   title = 'weather-app';
-  countryName: string = ''; 
-
+  countryName: string = 'egypt'; 
   weatherDetails: any = {};
 
  constructor(private _httpClient:HttpClient){}
+
+ ngOnInit(){
+    if (navigator.geolocation) {
+	      navigator.geolocation.watchPosition((position) => {
+        this._getWeatherDetails(position.coords.latitude,position.coords.longitude)
+	    });
+    }
+  }
 
   getCountryWeather(){
 
     this._httpClient.get<any>(`https://geocoding-api.open-meteo.com/v1/search?name=${this.countryName}`).subscribe((locationData)=>{
       var countryLatitude=locationData.results[0].latitude;
       var countryLongitude=locationData.results[0].longitude;
-      console.log(countryLatitude, countryLongitude)
       this._getWeatherDetails(countryLatitude, countryLongitude);
       
     })
   }
 
   private _getWeatherDetails(lat: number, lon:number){
-    this._httpClient.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}.52&longitude=${lon}.41&hourly=temperature_2m`).subscribe((weatherData: any)=>{
+    this._httpClient.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,sunrise,sunset,daylight_duration,sunshine_duration,rain_sum,showers_sum,snowfall_sum,precipitation_sum,precipitation_hours,precipitation_probability_max,temperature_2m_min&hourly=temperature_2m,weather_code,rain,precipitation,apparent_temperature,snowfall,showers`).subscribe((weatherData: any)=>{
       console.log(weatherData);
+      console.log(weatherData.daily.temperature_2m_max)
       this.weatherDetails = weatherData;
+      console.log(this.weatherDetails.daily)
     })
   }
-   
+
  
 }
